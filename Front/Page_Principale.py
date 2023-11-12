47b9c73f53c1ae9a5b461b4f83680c433f2505d7
Pierre-Louis <pierrelouis.thomas@edu.ece.fr>
2023-11-12 11:34:05 +0000

    Create Account + connection NICKEL!!

--- Front/Page_Principale.py
+++ Front/Page_Principale.py
@@ -21,11 +21,32 @@ class AIRENGLANDApp:
         self.email_label = tk.Label(self.login_frame, font=('Helvetica', 12), bg='black')
         self.email_label.place(relx=0.1, rely=0.5)
 
+
+        # Initialisation of first_name_label_create_account
+        self.first_name_label_create_account = tk.Label(self.login_frame, font=('Helvetica', 12), bg='black')
+        self.first_name_label_create_account.place(relx=0.1, rely=0.5)
+
+        # Initialisation of last_name_label_create_account
+        self.last_name_label_create_account = tk.Label(self.login_frame, font=('Helvetica', 12), bg='black')
+        self.last_name_label_create_account.place(relx=0.1, rely=2.5)
+
+        # Initialisation of category_label_create_account
+        self.category_label_create_account = tk.Label(self.login_frame, font=('Helvetica', 12), bg='black')
+        self.category_label_create_account.place(relx=0.1, rely=4.5)
+
+        # Initialisation of email_label_create_account
+        self.email_label_create_account = tk.Label(self.login_frame, font=('Helvetica', 12), bg='black')
+        self.email_label_create_account.place(relx=0.1, rely=6.5)
+
+        # Initialisation of password_label_create_account
+        self.password_label_create_account = tk.Label(self.login_frame, font=('Helvetica', 12), bg='black')
+        self.password_label_create_account.place(relx=0.1, rely=8.5)
+
         self.root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
         self.root.title("AIR ENGLAND: Home Page")
         self.bandeau_height = root.winfo_screenheight() * 0.22
         self.menu = None
-        self.bouton_connexion = None
+        self.bouton_connection = None
         self.bouton_create_account = None
 
         self.create_bandeau()
@@ -113,44 +134,44 @@ class AIRENGLANDApp:
         except Exception as e:
             messagebox.showerror("Error", f"Error on redirection {e}")
 
-    def open_connexion_window(self):
-        self.connexion_window = tk.Toplevel(self.root)
-        self.connexion_window.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
-        self.connexion_window.title("Connexion")
+    def open_connection_window(self):
+        self.connection_window = tk.Toplevel(self.root)
+        self.connection_window.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
+        self.connection_window.title("connection")
 
         # Widgets pour l'email
-        email_label = tk.Label(self.connexion_window, text="Email")
+        email_label = tk.Label(self.connection_window, text="Email")
         email_label.pack()
-        self.email_entry = tk.Entry(self.connexion_window)
+        self.email_entry = tk.Entry(self.connection_window)
         self.email_entry.pack()
 
         # Widgets pour le mot de passe
-        password_label = tk.Label(self.connexion_window, text="Password")
+        password_label = tk.Label(self.connection_window, text="Password")
         password_label.pack()
-        self.password_entry = tk.Entry(self.connexion_window, show="*")
+        self.password_entry = tk.Entry(self.connection_window, show="*")
         self.password_entry.pack()
 
-        login_button = tk.Button(self.connexion_window, text="Connexion", command=self.login)
+        login_button = tk.Button(self.connection_window, text="connection", command=self.login)
         login_button.pack()
 
-        login_button = tk.Button(self.connexion_window, text="Create an account", command=self.login)
-        login_button.pack()
+        create_account_button = tk.Button(self.create_account_window, text="Create an account", command=self.create_account)
+        create_account_button.pack()
 
         # Message d'erreur
-        self.error_label = tk.Label(self.connexion_window, text="", fg="red")
+        self.error_label = tk.Label(self.connection_window, text="", fg="red")
         self.error_label.pack()
 
     def login(self):
         email = self.email_entry.get()
         password = self.password_entry.get()
 
-        # Remplacez ceci par votre propre logique de vérification de la connexion
+        # Remplacez ceci par votre propre logique de vérification de la connection
         success, user_info = self.verify_login(email, password)
 
         if success:
             config.is_user_logged_in = True
             config.user_type, config.user_last_name, config.user_first_name, config.client_type = user_info
-            self.connexion_window.destroy()  # Ferme uniquement la fenêtre de connexion
+            self.connection_window.destroy()  # Ferme uniquement la fenêtre de connection
             self.update_ui()
             self.periodic_update()
         else:
@@ -165,7 +186,9 @@ class AIRENGLANDApp:
         )
         try:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT First_Name, Last_Name, Type, Category FROM User WHERE email = %s AND password = %s", (email, password))
+                hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+                cursor.execute("SELECT First_Name, Last_Name, Type, Category FROM User WHERE email = %s AND password = %s", (email, hashed_password))
                 result = cursor.fetchone()
                 if result:
                     # Créer un dictionnaire avec les informations de l'utilisateur
@@ -188,44 +211,42 @@ class AIRENGLANDApp:
 # Create account
     def open_create_account_window(self):
         self.create_account_window = tk.Toplevel(self.root)
-        #self.connexion_window.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
+        #self.connection_window.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}")
         self.create_account_window.title("Create an account")
 
-        # Cadre pour le formulaire de création de compte
-        account_frame = tk.Frame(root, bg='white', bd=5)
-        account_frame.place(relx=0.5, rely=0.5, relwidth=0.8, relheight=0.8, anchor='center')
-
-        # Style pour les labels et les entrées
-        label_font = ('Verdana', 12)
-        entry_font = ('Verdana', 12)
-        button_font = ('Verdana', 12, 'bold')
-
         # First name
-        tk.Label(account_frame, text="First name", font=label_font, bg='black').pack(pady=10)
-        first_name_entry = tk.Entry(account_frame, font=entry_font)
-        first_name_entry.pack(fill='x', padx=50)
+        tk.Label(self.create_account_window, text="First name").pack(pady=10)
+        self.first_name_entry = tk.Entry(self.create_account_window)
+        self.first_name_entry.pack(fill='x', padx=50)
 
         # Last name
-        tk.Label(account_frame, text="Last name", font=label_font, bg='black').pack(pady=10)
-        last_name_entry = tk.Entry(account_frame, font=entry_font)
-        last_name_entry.pack(fill='x', padx=50)
+        tk.Label(self.create_account_window, text="Last name").pack(pady=10)
+        self.last_name_entry = tk.Entry(self.create_account_window)
+        self.last_name_entry.pack(fill='x', padx=50)
 
         # Category
-        tk.Label(account_frame, text="Category", font=label_font, bg='black').pack(pady=10)
-        category_var = tk.StringVar(value="regular")  # default value
-        category_option_menu = ttk.OptionMenu(account_frame, category_var, "regular", "regular", "senior", "child")
-        category_option_menu.pack(fill='x', padx=50)
+        tk.Label(self.create_account_window, text="Category").pack(pady=10)
+        self.category_var = tk.StringVar(value="regular")  # default value
+        self.category_option_menu = ttk.OptionMenu(self.create_account_window, self.category_var, "regular", "regular", "senior", "child")
+        self.category_option_menu.pack(fill='x', padx=50)
 
         # Email
-        tk.Label(account_frame, text="Email", font=label_font, bg='black').pack(pady=10)
-        email_entry = tk.Entry(account_frame, font=entry_font)
-        email_entry.pack(fill='x', padx=50)
+        tk.Label(self.create_account_window, text="Email").pack(pady=10)
+        self.email_entry = tk.Entry(self.create_account_window)
+        self.email_entry.pack(fill='x', padx=50)
 
         # Password
-        tk.Label(account_frame, text="Password", font=label_font, bg='black').pack(pady=10)
-        password_entry = tk.Entry(account_frame, font=entry_font, show="*")
-        password_entry.pack(fill='x', padx=50)
+        tk.Label(self.create_account_window, text="Password").pack(pady=10)
+        self.password_entry = tk.Entry(self.create_account_window, show="*")
+        self.password_entry.pack(fill='x', padx=50)
         # Par exemple, des Entry pour le prénom, le nom, l'email, etc.
+
+        login_button = tk.Button(self.create_account_window, text="Create an account", command=self.create_account)
+        login_button.pack()
+
+        # Message d'erreur
+        self.error_label = tk.Label(self.create_account_window, text="", fg="red")
+        self.error_label.pack()
 
     def email_exists(self, email):
         conn = pymysql.connect(
@@ -241,7 +262,7 @@ class AIRENGLANDApp:
                 cursor.execute(sql, (email,))
                 return cursor.fetchone() is not None
         except Exception as e:
-            messagebox.showerror("Erreur", f"Database error: {e}")
+            self.error_label.config(text=f"Database error: {e}")
             return False
         finally:
             conn.close()
@@ -256,12 +277,13 @@ class AIRENGLANDApp:
         try:
             with conn.cursor() as cursor:
                 hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-                sql = "INSERT INTO User (First_Name, Last_Name, Category, Email, Password) VALUES (%s, %s, %s, %s, %s)"
-                cursor.execute(sql, (first_name, last_name, client_type, category, email,
-                                     password))  # remplacer password par hashed_password
+                sql = "INSERT INTO User (First_Name, Last_Name, Type, Category, Email, Password) VALUES (%s, %s, %s, %s, %s, %s)"
+                cursor.execute(sql, (first_name, last_name, 'Member', category, email, hashed_password)) # remplacer password par hashed_password
             conn.commit()
+
         except Exception as e:
-            messagebox.showerror("Erreur", f"Database error: {e}")
+            self.error_label.config(text=f"Database error: {e}")
+            return
         finally:
             conn.close()
     def create_account(self):
@@ -272,10 +294,13 @@ class AIRENGLANDApp:
         password = self.password_entry.get()
 
         if self.email_exists(email):
-            messagebox.showerror("Erreur", "Email already exists!")
+            self.error_label.config(text="Email already exists!")
+            return
+        if not first_name or not last_name or not category or not email or not password:
+            self.error_label.config(text="Please enter all fields")
             return
 
-        self.insert_client(first_name, last_name, category, email, password)
+        self.insert_client(first_name, last_name, 'Member', category, email, password)
         messagebox.showinfo("Success", "Account created successfully!")
         self.create_account_window.destroy()
 
@@ -294,24 +319,24 @@ class AIRENGLANDApp:
             self.login_frame.place(relx=1, rely=0, relwidth=0.25, relheight=0.1, anchor='ne')
             self.email_label.config(text=user_info_text)
 
-            if self.bouton_connexion is not None:
-                self.bouton_connexion.destroy()
-                self.bouton_connexion = None
+            if self.bouton_connection is not None:
+                self.bouton_connection.destroy()
+                self.bouton_connection = None
             if self.bouton_create_account is not None:
                 self.bouton_create_account.destroy()
                 self.bouton_create_account = None
 
         else:
-            if self.bouton_connexion is None:
-                self.bouton_connexion = tk.Button(self.root, text="Connexion", width=15, command=self.open_connexion_window)
-                self.bouton_connexion.place(x=self.bandeau_height * 5.4, y=self.bandeau_height * 0.8)
-                self.bouton_connexion.bind('<Enter>', self.bouton_hover)
-                self.bouton_connexion.bind('<Leave>', self.bouton_leave)
+            if self.bouton_connection is None:
+                self.bouton_connection = tk.Button(self.root, text="connection", width=15, command=self.open_connection_window)
+                self.bouton_connection.place(x=self.bandeau_height * 5.4, y=self.bandeau_height * 0.8)
+                self.bouton_connection.bind('<Enter>', self.bouton_hover)
+                self.bouton_connection.bind('<Leave>', self.bouton_leave)
             if self.bouton_create_account is None:
-                self.bouton_creer_compte = tk.Button(self.root, text="Create an account", width=15,command=self.redirect_to_create)
-                self.bouton_creer_compte.place(x=self.bandeau_height * 6.2, y=self.bandeau_height * 0.8)
-                self.bouton_creer_compte.bind('<Enter>', self.bouton_hover)
-                self.bouton_creer_compte.bind('<Leave>', self.bouton_leave)
+                self.bouton_create_account = tk.Button(self.root, text="Create an account", width=15,command=self.open_create_account_window)
+                self.bouton_create_account.place(x=self.bandeau_height * 6.2, y=self.bandeau_height * 0.8)
+                self.bouton_create_account.bind('<Enter>', self.bouton_hover)
+                self.bouton_create_account.bind('<Leave>', self.bouton_leave)
 
     def periodic_update(self):
         self.update_ui()
