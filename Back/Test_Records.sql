ec319c00f8b13386caa479098c729bd7225db7f3
Pierre-Louis <pierrelouis.thomas@edu.ece.fr>
2023-11-21 14:11:21 +0000

    New BDD With flight upgrade

--- Back/Test_Records.sql
+++ Back/Test_Records.sql
@@ -12,28 +12,66 @@
 --       ('Jack', 'David', 'Member', 'regular', 'jack.david@example.com', 'jack1213');
 
 -- Inserting data into the Flight table
-INSERT INTO Flight (Departure_Date, Arrival_Date, Departure_Airport, Arrival_Airport, Price)
-VALUES ('2023-11-01 10:00:00', '2023-11-01 12:30:00', 'Paris CDG', 'London Heathrow', 100.50),
-       ('2023-11-02 15:30:00', '2023-11-02 18:00:00', 'New York JFK', 'Paris CDG', 350.75),
-       ('2023-11-03 20:00:00', '2023-11-04 08:00:00', 'Tokyo Haneda', 'Los Angeles LAX', 550.20),
-       ('2023-11-04 06:00:00', '2023-11-04 09:00:00', 'Berlin Tegel', 'Rome Fiumicino', 120.80),
-       ('2023-11-05 12:00:00', '2023-11-05 14:30:00', 'Sydney Kingsford', 'Auckland', 200.60),
-       ('2023-11-06 13:00:00', '2023-11-06 16:30:00', 'Cairo', 'Johannesburg', 250.10),
-       ('2023-11-07 09:00:00', '2023-11-07 11:30:00', 'Moscow Sheremetyevo', 'Istanbul', 170.90),
-       ('2023-11-08 07:00:00', '2023-11-08 10:00:00', 'Rio de Janeiro', 'Buenos Aires', 180.00),
-       ('2023-11-09 18:00:00', '2023-11-09 21:00:00', 'Beijing', 'Seoul Incheon', 220.35),
-       ('2023-11-10 16:30:00', '2023-11-10 20:00:00', 'Mumbai', 'Dubai', 300.50);
+DELIMITER //
+
+CREATE PROCEDURE InsertFlightsForNovember()
+BEGIN
+    DECLARE dayCounter INT DEFAULT 1;
+    DECLARE flightCounter INT;
+    DECLARE departureTime TIME;
+    DECLARE arrivalTime TIME;
+    DECLARE departureAirport VARCHAR(255);
+    DECLARE arrivalAirport VARCHAR(255);
+    DECLARE price DECIMAL(10,2);
+
+    DECLARE airportList VARCHAR(255) DEFAULT 'Paris CDG,New York JFK,Tokyo Haneda,Berlin Tegel,Sydney Kingsford,Cairo,Moscow Sheremetyevo,Rio de Janeiro,Beijing,Mumbai';
+    DECLARE airportArray VARCHAR(255);
+
+    WHILE dayCounter <= 30 DO
+        SET flightCounter = 1;
+
+        WHILE flightCounter <= 10 DO
+            SET departureTime = ADDTIME('06:00:00', SEC_TO_TIME(RAND() * 36000)); -- entre 06:00 et 16:00
+            SET arrivalTime = ADDTIME(departureTime, '02:00:00'); -- ajout de 2 heures pour l'heure d'arrivée
+
+            SET departureAirport = SUBSTRING_INDEX(SUBSTRING_INDEX(airportList, ',', 1 + FLOOR(RAND() * 10)), ',', -1);
+            SET arrivalAirport = SUBSTRING_INDEX(SUBSTRING_INDEX(airportList, ',', 1 + FLOOR(RAND() * 10)), ',', -1);
+            WHILE arrivalAirport = departureAirport DO
+                SET arrivalAirport = SUBSTRING_INDEX(SUBSTRING_INDEX(airportList, ',', 1 + FLOOR(RAND() * 10)), ',', -1);
+            END WHILE;
+
+            SET price = 100.0 + RAND() * 200;
+
+            INSERT INTO Flight (Departure_Date, Arrival_Date, Departure_Airport, Arrival_Airport, Price)
+            VALUES
+                (CONCAT('2023-11-', LPAD(dayCounter, 2, '0'), ' ', departureTime),
+                 CONCAT('2023-11-', LPAD(dayCounter, 2, '0'), ' ', arrivalTime),
+                 departureAirport,
+                 arrivalAirport,
+                 price);
+
+            SET flightCounter = flightCounter + 1;
+        END WHILE;
+
+        SET dayCounter = dayCounter + 1;
+    END WHILE;
+END //
+
+DELIMITER ;
+
+CALL InsertFlightsForNovember();
+
 
 -- Inserting data into the Reservation table
 -- Assuming User_ID and Flight_ID auto-increment starts from 1
 INSERT INTO Reservation (User_ID, Flight_ID, Number_of_Tickets, Total_Payment)
-VALUES (1, 1, 2, 201.00),
-       (2, 2, 1, 350.75),
-       (3, 3, 4, 2200.80),
-       (4, 4, 3, 362.40),
-       (5, 5, 2, 401.20),
-       (6, 6, 1, 250.10),
-       (7, 7, 3, 512.70),
-       (8, 8, 4, 720.00),
-       (9, 9, 2, 440.70),
-       (10, 10, 1, 300.50);
+VALUES (1, 1, 2, 546.7),
+       (2, 2, 1, 167.72),
+       (3, 3, 4, 1146.52),
+       (4, 4, 3, 687.3),
+       (5, 5, 2, 525.82),
+       (6, 6, 1, 121.63),
+       (7, 7, 3, 374.07),
+       (8, 8, 4, 859.64),
+       (9, 9, 2, 230.26),
+       (10, 10, 1, 180.88);
