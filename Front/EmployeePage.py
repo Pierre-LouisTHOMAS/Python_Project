d43a428774c4518cc0df1fb20dcb1f0577cb12a5
Pierre-Louis <pierrelouis.thomas@edu.ece.fr>
2023-11-22 19:17:18 +0000

    flight page

--- Front/EmployeePage.py
+++ Front/EmployeePage.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkcalendar import DateEntry
-from tkinter import messagebox, ttk
+from tkinter import Toplevel, messagebox, ttk, Label
+from PIL import Image, ImageTk
 import AccountInformation
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -26,6 +27,23 @@ class HomeEmployee:
         self.create_header()
 
     def redirect_to_home_page(self, event):
+        config.is_user_logged_in = False
+        config.user_email = None
+        config.user_id = None
+        config.first_name_user = None
+        config.last_name_user = None
+        config.user_type = 0
+        config.member_category = 0
+        config.flight_data = []
+        config.departure_date = None
+        config.departure_airport = None
+        config.arrival_airport = None
+        config.selected_flight_id = None
+        config.selected_departure_date = None
+        config.selected_arrival_date = None
+        config.selected_departure_airport = None
+        config.selected_arrival_airport = None
+        config.selected_price = None
         self.root.destroy()
 
     def create_header(self):
@@ -78,7 +96,7 @@ class HomeEmployee:
             #under Menu name
             flight_menu.add_command(label="flight available", command=self.window_flight_available)
             flight_menu.add_command(label="flight discount offer", command=self.save)
-            flight_menu.add_command(label="add new flight", command=self.save)
+            flight_menu.add_command(label="add new flight", command=self.add_flight_window)
             flight_menu.add_command(label="number of tickets purchased", command=self.window_flight_available)
             customer_menu.add_command(label="Customer file management", command=self.window_file_management)
             customer_menu.add_command(label="Customer reservation history", command=self.window_history_reservation)
@@ -162,10 +180,11 @@ class HomeEmployee:
     def window_flight_available(self):
         client_window = tk.Toplevel(self.root)
         client_window.title("Flight available")
-        client_window.geometry("300x200")
+        client_window.geometry("800x300")
         client_window.configure(bg="white")
         self.main_frame = tk.Frame(client_window, relief="solid", borderwidth=2)
         self.main_frame.pack(padx=10, pady=10)
+
 
         conn = pymysql.connect(
             host='localhost',
@@ -209,8 +228,15 @@ class HomeEmployee:
 
         self.error_label = tk.Label(self.main_frame, text="", fg="red")
         self.error_label.grid(row=4, column=0, columnspan=2, pady=5)
-        self.submit_button = tk.Button(self.main_frame, text="Research", command=self.redirect_to_edit_flight)
-        self.submit_button.grid(row=4, column=0, columnspan=2, pady=10)
+
+        self.search_button = tk.Button(self.main_frame, text="Flight Research", command=self.redirect_to_Flight_booking)
+        self.search_button.grid(row=6, column=0, columnspan=2, pady=10)
+
+        image_path = "../Pictures/avionResa.png"
+        self.image = tk.PhotoImage(file=image_path)
+        self.image = self.image.subsample(2)
+        image_label = Label(self.main_frame, image=self.image)
+        image_label.grid(row=0, column=2, rowspan=7, padx=10)
 
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
@@ -221,10 +247,88 @@ class HomeEmployee:
     def check_airport_selection(self, event):
         if self.departure_var.get() == self.arrival_var.get():
             self.error_label.config(text="You can't have the same Departure Airport and Arrival Airport")
-            self.submit_button['state'] = 'disabled'
+            self.search_button['state'] = 'disabled'
         else:
             self.error_label.config(text="")
-            self.submit_button['state'] = 'normal'
+            self.search_button['state'] = 'normal'
+    def search_flights(self):
+        date = self.date_var.get()
+        departure = self.departure_var.get()
+        arrival = self.arrival_var.get()
+        person_type = self.person_type_var.get()
+        print(f"Date: {date}, Départ: {departure}, Arrivée: {arrival}, Type de Personne: {person_type}")
+    def redirect_to_Flight_booking(self):
+        try:
+            config.departure_date = self.date_var.get()
+            config.departure_airport = self.departure_var.get()
+            config.arrival_airport = self.arrival_var.get()
+
+            self.flightBooking_window = tk.Toplevel(self.root)
+            self.app = FlightBooking.FlightSelectionPage(self.flightBooking_window, config.departure_date, config.departure_airport, config.arrival_airport)
+        except Exception as e:
+            messagebox.showerror("Error", f"Error on redirection {e}")
+
+    def add_flight_window(self):
+        add_flight_window = tk.Toplevel(self.root)
+        add_flight_window.title("Add a new flight")
+        add_flight_window.geometry("400x300")
+        add_flight_window.configure(bg="white")
+        main_frame = tk.Frame(add_flight_window, relief="solid", borderwidth=2)
+        main_frame.pack(padx=10, pady=10)
+
+        # Widgets pour la saisie des informations
+        departure_label = tk.Label(main_frame, text="Departure airport")
+        departure_label.grid(row=0, column=0, pady=5)
+        departure_entry = tk.Entry(main_frame)
+        departure_entry.grid(row=0, column=1, pady=5)
+
+        arrival_label = tk.Label(main_frame, text="Arrival airport")
+        arrival_label.grid(row=1, column=0, pady=5)
+        arrival_entry = tk.Entry(main_frame)
+        arrival_entry.grid(row=1, column=1, pady=5)
+
+        departure_date_label = tk.Label(main_frame, text="Departure date")
+        departure_date_label.grid(row=2, column=0, pady=5)
+        departure_date_entry = DateEntry(main_frame, date_pattern="yyyy-mm-dd")
+        departure_date_entry.grid(row=2, column=1, pady=5)
+
+        arrival_date_label = tk.Label(main_frame, text="Arrival date")
+        arrival_date_label.grid(row=3, column=0, pady=5)
+        arrival_date_entry = DateEntry(main_frame, date_pattern="yyyy-mm-dd")
+        arrival_date_entry.grid(row=3, column=1, pady=5)
+
+        price_label = tk.Label(main_frame, text="Price")
+        price_label.grid(row=4, column=0, pady=5)
+        price_entry = tk.Entry(main_frame)
+        price_entry.grid(row=4, column=1, pady=5)
+
+        add_button = tk.Button(main_frame, text="Add a new flight", command=lambda: self.ajouter_vol(
+            departure_entry.get(),
+            arrival_entry.get(),
+            departure_date_entry.get(),
+            arrival_date_entry.get(),
+            price_entry.get()
+        ))
+        add_button.grid(row=5, column=0, columnspan=2, pady=10)
+    def ajouter_vol(self, departure_airport, arrival_airport, departure_date, arrival_date, price):
+        try:
+            conn = pymysql.connect(
+                host='localhost',
+                user='root',
+                password='root',
+                db='AirlineDatabase',
+                port=8889
+            )
+            with conn.cursor() as cursor:
+                sql = "INSERT INTO Flight (Departure_Airport, Arrival_Airport, Departure_Date, Arrival_Date, Price) VALUES (%s, %s, %s, %s, %s)"
+                cursor.execute(sql, (departure_airport, arrival_airport, departure_date, arrival_date, price))
+            conn.commit()
+            messagebox.showinfo("Success", "Flight added with success!")
+        except Exception as e:
+            messagebox.showerror("Error", f"An error occur : {e}")
+        finally:
+            if conn:
+                conn.close()
 
     def sales_analysis(self):
         try:
